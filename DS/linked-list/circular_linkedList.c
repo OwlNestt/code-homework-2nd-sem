1691ea037961066d623615b95a17b4c477f32644
Rakesh <149894070+rakeshdeep@users.noreply.github.com>
2025-06-22 18:20:35 +0530

    ok

---
+++ DS/linked-list/circular_linkedList.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+int main(){
+    struct node *newNode, *head, *temp;
+    int ch;
+    head = NULL;
+    while(ch != 0){
+        newNode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter a value: ");
+        scanf("%d", &newNode->data);
+        newNode->next = head;
+        if(head == NULL){
+           temp = head = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+        printf("want to continue (0/1): ");
+        scanf("%d",&ch);
+    }
+    if (head != NULL) {
+        temp->next = head;
+    }
+    temp = head;
+    if (head != NULL) {
+        do {
+            printf("%d->", temp->data);
+            temp = temp->next;
+        } while (temp != head);
+    }
+
+    return 0;
+}
